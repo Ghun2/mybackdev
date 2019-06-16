063377db4da5705e624a0c322c812f60101fb664
ghun2ee@gmail.com <ghun@gimjihun-ui-MacBook-Pro-2.local>
2019-06-16 16:50:47 +0900

    Schematics validation을 위한 view decorator 추가

--- app/hooks/error.py
+++ app/hooks/error.py
@@ -1,5 +1,11 @@
+from flask import jsonify
+from schematics.exceptions import BaseError
 from werkzeug.exceptions import HTTPException
 
+def schematics_baseerror_handler(e: BaseError):
+    return jsonify({
+        'msg': e.to_primitive()
+    }), 400
 
 def http_exception_handler(e: HTTPException):
     return '', e.code
