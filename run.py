e198f4b53e5392024d817b9873d2fafb51151129
ghun2ee@gmail.com <ghun@gimjihun-ui-MacBook-Pro-2.local>
2019-06-16 16:43:36 +0900

    Flask Large Application Example 적용

---
+++ run.py
@@ -0,0 +1,9 @@
+from app import create_app
+from config.app_config import LocalLevelConfig
+from config.db_config import LocalDBConfig
+from constants.local_run import RUN_SETTING
+
+app = create_app(LocalLevelConfig, LocalDBConfig)
+
+if __name__ == '__main__':
+    app.run(**RUN_SETTING)
